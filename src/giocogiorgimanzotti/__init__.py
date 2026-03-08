3b16d2ded9a2c0589cb3ca59b6c8e1f10a0467c1
poll0904 <paologiorgi04@gmail.com>
2026-03-08 16:08:50 +0100

    penultimo3.0

--- src/giocogiorgimanzotti/__init__.py
+++ src/giocogiorgimanzotti/__init__.py
@@ -3,11 +3,11 @@ import math
 import random
 
 
-from Costanti_e_variabili import *
-from Piattaforme import *
-from Lottatori import *
-from Menu_e_interfaccia import *
-from Funzione_principale import *
+from costanti_e_variabili import *
+from piattaforme import *
+from lottatori import *
+from menu_e_interfaccia import *
+from funzione_principale import *
 
 if __name__ == "__main__":
     main()
